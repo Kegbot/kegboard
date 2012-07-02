305deb8d2ebfd0834dc656453c9237555f7e9d48
mike wakerly <opensource@hoho.com>
2012-07-01 18:25:42 -0700

    Move Kegboard Python code into kegboard repository.
    New package is called "kegbot-kegboard".
    
    Origin:
      URL: https://github.com/Kegbot/kegbot
      Commit: a0fc59b9257eb6ff247ba4ff54b1180a675f13bb

---
+++ python/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""Kegboard support library.
+
+This package contains the Python protocol support for Kegboard.  For more
+information on Kegboard, see http://kegbot.org/kegboard/
+"""
+
+DOCLINES = __doc__.split('\n')
+
+# Change this to True to include optional dependencies
+USE_OPTIONAL = False
+
+VERSION = '1.0.0'
+SHORT_DESCRIPTION = DOCLINES[0]
+LONG_DESCRIPTION = '\n'.join(DOCLINES[2:])
+
+def setup_package():
+  from distribute_setup import use_setuptools
+  use_setuptools()
+  from setuptools import setup, find_packages
+
+  setup(
+      name = 'kegbot-kegboard',
+      version = VERSION,
+      description = SHORT_DESCRIPTION,
+      long_description = LONG_DESCRIPTION,
+      author = 'mike wakerly',
+      author_email = 'opensource@hoho.com',
+      url = 'http://kegbot.org/',
+      packages = find_packages(exclude=['testdata']),
+      namespace_packages = ['kegbot'],
+      scripts = [
+        'distribute_setup.py',
+        'bin/kegboard-monitor.py',
+        'bin/kegboard-tester.py',
+      ],
+      install_requires = [
+        'kegbot-pyutils >= 0.1.2',
+        'python-gflags',
+        'pyserial',
+      ],
+      include_package_data = True,
+  )
+
+if __name__ == '__main__':
+  setup_package()
